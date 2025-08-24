fb6332e76699feccd2b53e3e390c433d5a5370e0
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-24 13:17:37 -0300

    feat: criando o repository: CurrencyRepository

---
+++ ExchangeRate/ExchangeRate.Infra.Data/Repository/Currency/CurrencyRepository.cs
@@ -0,0 +1,26 @@
+﻿using ExchangeRate.Domain.Entity.Currency;
+using ExchangeRate.Domain.Interface;
+using ExchangeRate.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Infra.Data.Repository.Currency
+{
+    public class CurrencyRepository : ICurrencyRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public CurrencyRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreateCurrency(CurrencyInfo currencyInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
