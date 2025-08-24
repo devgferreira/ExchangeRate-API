8c8f30ffc474aebbbc42e2f73ec0ac4f3a8506a6
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-24 11:59:44 -0300

    feat: criando a interface: ICurrencyRepository

---
+++ ExchangeRate/ExchangeRate.Domain/Interface/ICurrencyRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExchangeRate.Domain.Entity.Currency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Domain.Interface
+{
+    public interface ICurrencyRepository
+    {
+    }
+}
