0da01ac6369cf198b691c69f8ff2035fc4505511
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-23 21:02:22 -0300

    feat: criando a interface da service: AwesomeAPIService

---
+++ ExchangeRate/ExchangeRate.Application/Interface/AwesomeAPI/IAwesomeAPIService.cs
@@ -0,0 +1,14 @@
+﻿using ExchangeRate.Application.DTO.Currency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Application.Interface.AwesomeAPI
+{
+    public interface IAwesomeAPIService
+    {
+        Task<CurrencyDTO> GetCurrencyAsync(string coin);
+    }
+}
