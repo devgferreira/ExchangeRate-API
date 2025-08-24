63d6ca7f14bd63d5536778c7faed00ed505c7807
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-23 21:41:10 -0300

    feat: criando a controller para AwesomeAPI

---
+++ ExchangeRate/ExchangeRate.API/Controllers/AwesomeAPI/AwesomeAPIController.cs
@@ -0,0 +1,31 @@
+﻿using ExchangeRate.Application.Interface.AwesomeAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRate.API.Controllers.AwesomeAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AwesomeAPIController : Controller
+    {
+        private readonly IAwesomeAPIService _awesomeAPIService;
+
+        public AwesomeAPIController(IAwesomeAPIService awesomeAPIService)
+        {
+            _awesomeAPIService = awesomeAPIService;
+        }
+
+        [HttpGet("{coin}")]
+        public async Task<IActionResult> GetCurrency(string coin)
+        {
+            try
+            {
+                var result  = await _awesomeAPIService.GetCurrencyAsync(coin);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
