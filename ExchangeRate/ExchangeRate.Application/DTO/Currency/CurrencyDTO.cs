e565c04ed931f2ac7f0c7a96dc2c4d007ba6ea1e
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-24 13:56:02 -0300

    feat: criando o DTO: CurrencyDTO

---
+++ ExchangeRate/ExchangeRate.Application/DTO/Currency/CurrencyDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Application.DTO.Currency
+{
+    public class CurrencyDTO
+    {
+        public string Symbol { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public DateTime DateOfCurrency { get; set; }
+        public DateTime CreatedAT { get; set; }
+    }
+}
