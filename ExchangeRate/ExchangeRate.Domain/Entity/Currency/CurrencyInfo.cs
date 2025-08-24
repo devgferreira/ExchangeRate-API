f668414aae26e2cac0ea358d6680318f2e888762
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-24 11:04:13 -0300

    feat: criando a entidade: CurrencyInfo

---
+++ ExchangeRate/ExchangeRate.Domain/Entity/Currency/CurrencyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Domain.Entity.Currency
+{
+    public class CurrencyInfo
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public DateTime Date { get; set; } 
+    }
+}
