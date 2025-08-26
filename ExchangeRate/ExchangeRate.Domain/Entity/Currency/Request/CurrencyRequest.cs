cc302143e23faa42fbed5eac4514e6cd0a593f84
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-26 15:50:00 -0300

    feat: criando o request: CurrencyRequest

---
+++ ExchangeRate/ExchangeRate.Domain/Entity/Currency/Request/CurrencyRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Domain.Entity.Currency.Request
+{
+    public class CurrencyRequest
+    {
+        public string? Code { get; set; }
+        public string? CodeIn { get; set; }
+        public string DateOfCurrency { get; set; }
+
+    }
+}
