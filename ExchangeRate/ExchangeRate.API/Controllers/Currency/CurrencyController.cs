c6492eee615e35ae9cec4ee025fc1e7451dd047f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-26 16:51:12 -0300

    feat: criando a controller: CurrencyController

---
+++ ExchangeRate/ExchangeRate.API/Controllers/Currency/CurrencyController.cs
@@ -0,0 +1,32 @@
+﻿using ExchangeRate.Application.Interface.Currency;
+using ExchangeRate.Domain.Entity.Currency.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRate.API.Controllers.Currency
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class CurrencyController : Controller
+    {
+        private readonly ICurrencyService _currencyService;
+
+        public CurrencyController(ICurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+        }
+        [HttpGet("CalculatePriceBidVariationOnTheDay")]
+        public async Task<IActionResult> CurrencyCalculatePriceBidVariationOnTheDay([FromQuery]CurrencyRequest request)
+        {
+            try
+            {
+                var result = await _currencyService.CurrencyCalculatePriceBidVariationOnTheDay(request);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
