1b0b55df779840f26b23b61ee816ab27c7c7783c
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-27 19:25:35 -0300

    fix: ajustando as pasta

--- ExchangeRate/ExchangeRate.Test/Service/User/UserServiceTest.cs
+++ ExchangeRate/ExchangeRate.Test/Service/User/UserServiceTest.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExchangeRate.Test.Service
+namespace ExchangeRate_Test.Service.User
 {
     public class UserServiceTest
     {
