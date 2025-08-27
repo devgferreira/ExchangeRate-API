30d2d3c314c3530566a219515bfd09ac8c5a9a4e
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-27 15:50:43 -0300

    feat: criando a entidade: UserInfo

---
+++ ExchangeRate/ExchangeRate.Domain/Entity/User/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Domain.Entity.User
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
