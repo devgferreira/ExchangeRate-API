56c1600b0c1b3b3bf52b086d588a23cd49bf7f52
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2025-08-27 15:51:10 -0300

    refactor: colocando o ICurrencyRepository em uma pasta

---
+++ ExchangeRate/ExchangeRate.Domain/Interface/Currency/ICurrencyRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExchangeRate.Domain.Entity.Currency;
+using ExchangeRate.Domain.Entity.Currency.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Domain.Interface.Currency
+{
+    public interface ICurrencyRepository
+    {
+        Task<bool> CreateCurrency(CurrencyInfo currencyInfo);
+        Task<List<CurrencyInfo>> SelectCurrency(CurrencyRequest request);
+    }
+}
